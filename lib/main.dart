d6a5a675e0ac3f1f803bf71d0916ad7c83aed50e
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-31 12:00:53 +0900

    Expanded, Flexible & 상품 레이아웃 숙제

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.star),
-          title: Text('AppBar'),
-          actions: [
-            Icon(Icons.star),
-            Icon(Icons.star)
-          ],
-        ),
-        body: SizedBox(
-          child: IconButton(
-            icon: Icon(Icons.star),
-            onPressed: () {
-
-            },
+        appBar: AppBar(),
+        body: Container(
+          height: 150,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image.asset('assets/logo.png', width: 150,),
+              Expanded(
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('카메라팝니다.'),
+                      Text('금호동 3가'),
+                      Text('7,000원'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite),
+                          Text('4'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        // bottomNavigationBar: BottomAppBar(),
       ),
     );
   }
