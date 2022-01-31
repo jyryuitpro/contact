4b40d8a0404222e0975b1e66b8b9ef679f223bc1
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-31 17:39:44 +0900

    Dialog/모달창 만드는 법과 context가 뭔지

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
-// class Test extends StatefulWidget {
-//   const Test({Key? key}) : super(key: key);
-//
-//   @override
-//   _TestState createState() => _TestState();
-// }
-
-// class _TestState extends State<Test> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
-
-
 class MyApp extends StatefulWidget {
-
   MyApp({Key? key}) : super(key: key);
 
   @override
@@ -29,45 +17,45 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var a = 1;
+
   // var name = '연락처앱';
   var name = ['류지영', '홍길동', '피자집'];
   var like = [0, 0, 0];
 
+  //BuildContext context: 부모위젯의 정보를 담고있는 변수일 뿐
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('연락처앱'),
-        ),
-        body: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            print(index);
-            return ListTile(
-              // leading: Image.asset('assets/logo.png'),
-              leading: Text(like[index].toString()),
-              title: Text(name[index]),
-              trailing: ElevatedButton(
-                child: Text('좋아요'),
-                onPressed: () {
-                  setState(() {
-                    like[index]++;
-                  });
+    print(context.findAncestorWidgetOfExactType<MaterialApp>());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('연락처앱'),
+      ),
+      body: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Image.asset('assets/logo.png'),
+            title: Text(name[index]),
+          );
+        },
+      ),
+      //Builder: 족보생성기
+      floatingActionButton: Builder(
+        builder: (context) {
+          return FloatingActionButton(
+            child: Text(a.toString()),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: Text('안녕'),
+                  );
                 },
-              ),
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text(a.toString()),
-          onPressed: () {
-            print(a);
-            setState(() {
-              a++;
-            });
-          },
-        ),
+              );
+            },
+          );
+        }
       ),
     );
   }
