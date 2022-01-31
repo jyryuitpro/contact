4f742316f3b5fb4bb6c533d56e81e3be41c71f0a
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-31 16:55:55 +0900

    StatefulWidget 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,58 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  var a = 1;
+// class Test extends StatefulWidget {
+//   const Test({Key? key}) : super(key: key);
+//
+//   @override
+//   _TestState createState() => _TestState();
+// }
+
+// class _TestState extends State<Test> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
+
+class MyApp extends StatefulWidget {
 
   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  // var name = '연락처앱';
+  var name = ['류지영', '홍길동', '피자집'];
+  var like = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('연락처앱'),
+        ),
         body: ListView.builder(
           itemCount: 3,
           itemBuilder: (context, index) {
             print(index);
             return ListTile(
-              leading: Image.asset('assets/logo.png'),
-              title: Text('류지영'),
+              // leading: Image.asset('assets/logo.png'),
+              leading: Text(like[index].toString()),
+              title: Text(name[index]),
+              trailing: ElevatedButton(
+                child: Text('좋아요'),
+                onPressed: () {
+                  setState(() {
+                    like[index]++;
+                  });
+                },
+              ),
             );
           },
         ),
@@ -28,7 +63,9 @@ class MyApp extends StatelessWidget {
           child: Text(a.toString()),
           onPressed: () {
             print(a);
-            a++;
+            setState(() {
+              a++;
+            });
           },
         ),
       ),
