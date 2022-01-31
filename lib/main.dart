319c8e568c72338bea3459e9058cfaf83ff71bd5
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-31 14:00:40 +0900

    코드가 길어지면 커스텀 위젯만드세요

--- lib/main.dart
+++ lib/main.dart
@@ -12,35 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('assets/logo.png', width: 150,),
-              Expanded(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('카메라팝니다.'),
-                      Text('금호동 3가'),
-                      Text('7,000원'),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite),
-                          Text('4'),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+        // body: ShopItem(),
+        body: ListView(
+          // controller: ,
+          children: [
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+            Text('ListView'),
+          ],
         ),
+        // bottomNavigationBar: ,
       ),
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text('안녕'),
     );
   }
 }
